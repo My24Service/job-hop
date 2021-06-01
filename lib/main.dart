0294eb6aabd75e67e29c5629e68b9a24ec1907ab
Richard Jansen <richard@pedroja.tech>
2021-06-01 13:50:06 +0200

    implemented login/logout logic; apple login now working

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:jobhop/company/models/models.dart';
+import 'package:jobhop/utils/auth.dart';
+import 'package:jobhop/utils/state.dart';
+import 'package:jobhop/utils/apple.dart';
+import 'package:jobhop/utils/widgets.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      title: 'Job-Hop',
-      home: JobHopHome(),
+    ChangeNotifierProvider(
+      create: (context) => AppStateModel(),
+      child: MaterialApp(
+        title: 'Job-Hop',
+        home: JobHopHome(),
+      ),
     ),
   );
 }
@@ -16,7 +25,10 @@ class JobHopHome extends StatefulWidget {
 }
 
 class JobHopHomeState extends State<JobHopHome> {
-  StudentUser? _user;
+  Auth _auth = Auth();
+  Apple _apple = Apple();
+  String? _token;
+  bool _inAsyncCall = false;
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +38,10 @@ class JobHopHomeState extends State<JobHopHome> {
         ),
         body: ConstrainedBox(
           constraints: const BoxConstraints.expand(),
-          child: _buildBody(),
+          child: ModalProgressHUD(
+            child: _buildBody(),
+            inAsyncCall: _inAsyncCall
+          )
         ));
   }
 
@@ -37,28 +52,79 @@ class JobHopHomeState extends State<JobHopHome> {
   }
 
   _doAsync() async {
-    await _getUser();
+    await _setUserToken();
   }
 
-  Future<StudentUser> _getUser() async {
-    return null;
+  Future<String?> _setUserToken() async {
+    _token = await _auth.getUserToken();
   }
 
   Widget _buildBody() {
-    if (_user != null) {
+    if (_token != null) {
       // load orders or show menu?
       return Column(
-
+        children: [
+          Text('Yay you are logged in'),
+          ElevatedButton(
+              child: Text('Logout'),
+              onPressed: () async {
+                _inAsyncCall = true;
+                setState(() {});
+                await _auth.logout();
+                _inAsyncCall = false;
+                setState(() {});
+              }
+          )
+        ],
       );
     } else {
       return Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
-          const Text("You are not currently signed in."),
-          ElevatedButton(
-            child: const Text('SIGN IN'),
-            onPressed: () => {},
+          Image(image: AssetImage('assets/logo-big.png')),
+          Spacer(),
+          InkWell(
+            onTap: () {
+
+            },
+            child: Container(
+              child: ClipRRect(
+                child: Image(image: AssetImage('assets/buttons/facebook-sign-in.png')),
+              ),),
           ),
+          SizedBox(height: 20),
+          InkWell(
+            onTap: () {
+
+            },
+            child: Container(
+              child: ClipRRect(
+                child: Image(image: AssetImage('assets/buttons/btn_google_signin_dark_normal_web@2x.png')),
+              ),),
+          ),
+          SizedBox(height: 20),
+          InkWell(
+            onTap: () async {
+              _inAsyncCall = true;
+              setState(() {});
+              final bool result = await _apple.login();
+
+              if(!result) {
+                return displayDialog(context, 'Error logging in', 'There was an error logging you in using Apple');
+                _inAsyncCall = true;
+                setState(() {});
+              }
+
+              await _setUserToken();
+              _inAsyncCall = false;
+              setState(() {});
+            },
+            child: Container(
+              child: ClipRRect(
+                child: Image(image: AssetImage('assets/buttons/apple-id-sign-in-with.png')),
+              ),),
+          ),
+          SizedBox(height: 20),
         ],
       );
     }
