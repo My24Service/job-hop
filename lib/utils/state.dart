394f12e7c872d37b817c1e30f96ad39641f64b65
Richard Jansen <richard@pedroja.tech>
2021-06-01 13:43:00 +0200

    added AppStateModel for app state management

---
+++ lib/utils/state.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:jobhop/company/models/models.dart';
+
+class AppStateModel extends ChangeNotifier {
+  late StudentUser user;
+
+  AppStateModel();
+
+  void setUser(StudentUser _user) {
+    user = StudentUser(
+        username: _user.username,
+        email: _user.email,
+        token: _user.token,
+    );
+
+    notifyListeners();
+  }
+}
