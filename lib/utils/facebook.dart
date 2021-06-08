375715b3a05f56c71754213fa5cd568485e1e286
Richard Jansen <richard@pedroja.tech>
2021-06-08 07:39:29 +0200

    created a Facebook class from the example code

--- lib/utils/facebook.dart
+++ lib/utils/facebook.dart
@@ -1,134 +1,73 @@
 import 'dart:convert';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'auth.dart';
 
-String prettyPrint(Map json) {
-  JsonEncoder encoder = new JsonEncoder.withIndent('  ');
-  String pretty = encoder.convert(json);
-  return pretty;
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class Facebook {
   Map<String, dynamic>? _userData;
-  AccessToken? _accessToken;
-  bool _checking = true;
 
-  @override
-  void initState() {
-    super.initState();
-    _checkIfIsLogged();
-  }
-
-  Future<void> _checkIfIsLogged() async {
-    final accessToken = await FacebookAuth.instance.accessToken;
-    setState(() {
-      _checking = false;
-    });
-    if (accessToken != null) {
-      print("is Logged:::: ${prettyPrint(accessToken.toJson())}");
-      // now you can call to  FacebookAuth.instance.getUserData();
-      final userData = await FacebookAuth.instance.getUserData();
-      // final userData = await FacebookAuth.instance.getUserData(fields: "email,birthday,friends,gender,link");
-      _accessToken = accessToken;
-      setState(() {
-        _userData = userData;
-      });
-    }
-  }
-
-  void _printCredentials() {
-    print(
-      _accessToken != null ? prettyPrint(_accessToken!.toJson()) : '',
-    );
-  }
-
-  Future<void> _login() async {
+  Future<bool> login() async {
     final LoginResult result = await FacebookAuth.instance.login(); // by the fault we request the email and the public profile
 
-    // loginBehavior is only supported for Android devices, for ios it will be ignored
-    // final result = await FacebookAuth.instance.login(
-    //   permissions: ['email', 'public_profile', 'user_birthday', 'user_friends', 'user_gender', 'user_link'],
-    //   loginBehavior: LoginBehavior
-    //       .DIALOG_ONLY, // (only android) show an authentication dialog instead of redirecting to facebook app
-    // );
-
     if (result.status == LoginStatus.success) {
-      _accessToken = result.accessToken;
-      _printCredentials();
       // get the user data
       // by default we get the userId, email,name and picture
       final userData = await FacebookAuth.instance.getUserData();
       // final userData = await FacebookAuth.instance.getUserData(fields: "email,birthday,friends,gender,link");
+      AccessToken? _accessToken = result.accessToken;
       _userData = userData;
+
+      final registerByTokenEndpoint = Uri(
+        scheme: 'https',
+        host: 'jobhop.my24service-dev.com',
+        path: '/register-by-token/facebook/',
+      );
+
+      final Map<String, String> body = {
+        'access_token': _accessToken!.token,
+        'email': userData['email'],
+      };
+
+      final Map<String, String> headers = {"Content-Type": "application/json; charset=UTF-8"};
+
+      final response = await http.Client().post(
+        registerByTokenEndpoint,
+        body: json.encode(body),
+        headers: headers,
+      );
+
+      if(response.statusCode != 200) {
+        return false;
+      }
+
+      Map<String, dynamic> parsedResponse = json.decode(response.body);
+
+      if(parsedResponse['result'] == false) {
+        return false;
+      }
+
+      final Auth auth = Auth();
+      await auth.storeUser(parsedResponse);
+      await auth.storeBackend('facebook');
+
+      return true;
     } else {
       print(result.status);
       print(result.message);
+
+      return false;
     }
-
-    setState(() {
-      _checking = false;
-    });
   }
 
+  Future<void> logOut() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
 
-  Future<void> _logOut() async {
     await FacebookAuth.instance.logOut();
-    _accessToken = null;
-    _userData = null;
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Facebook Auth Example'),
-        ),
-        body: _checking
-            ? Center(
-          child: CircularProgressIndicator(),
-        )
-            : SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  _userData != null ? prettyPrint(_userData!) : "NO LOGGED",
-                ),
-                SizedBox(height: 20),
-                _accessToken != null
-                    ? Text(
-                  prettyPrint(_accessToken!.toJson()),
-                )
-                    : Container(),
-                SizedBox(height: 20),
-                CupertinoButton(
-                  color: Colors.blue,
-                  child: Text(
-                    _userData != null ? "LOGOUT" : "LOGIN",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: _userData != null ? _logOut : _login,
-                ),
-                SizedBox(height: 50),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+    await prefs.remove('email');
+    await prefs.remove('username');
+    await prefs.remove('token');
   }
 }
