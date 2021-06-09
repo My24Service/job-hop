f33c0fd31c0b25b6139394f031bbf22b66527396
Richard Jansen <richard@pedroja.tech>
2021-06-09 12:31:29 +0200

    added helper functions to utils

--- lib/utils/widgets.dart
+++ lib/utils/widgets.dart
@@ -12,3 +12,34 @@ Future<dynamic> displayDialog(context, title, text) {
       }
   );
 }
+
+Widget createHeader(String text) {
+  return Container(child: Column(
+    children: [
+      SizedBox(
+        height: 10.0,
+      ),
+      Text(text, style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+          color: Colors.grey
+      )),
+      SizedBox(
+        height: 10.0,
+      ),
+    ],
+  ));
+}
+
+createSnackBar(BuildContext context, String content) {
+  final snackBar = SnackBar(
+    content: Text(content),
+    duration: Duration(seconds: 1),
+  );
+
+  try {
+    Scaffold.of(context).showSnackBar(snackBar);
+  } catch(e) {
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+}
