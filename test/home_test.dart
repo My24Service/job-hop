3f79f6e1e326a64f791b11821d9d93a9a83b84a2
Richard Jansen <richard@pedroja.tech>
2021-06-09 14:07:01 +0200

    added basic home widget test

---
+++ test/home_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:jobhop/company/widgets/home.dart';
+
+
+Widget testWidget = MediaQuery(
+      data: MediaQueryData(),
+      child: MaterialApp(home: JobHopHome())
+);
+
+void main() {
+  testWidgets('Home widget shows login buttons', (WidgetTester tester) async {
+    await tester.pumpWidget(testWidget);
+
+    const facebookKey = Key('facebookButton');
+    expect(find.byKey(facebookKey), findsOneWidget);
+  });
+}
