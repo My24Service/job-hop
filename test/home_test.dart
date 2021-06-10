7544b51a31980c69b71f8b7c116412fede33f3a9
Richard Jansen <richard@pedroja.tech>
2021-06-10 20:11:43 +0200

    import fix

--- test/home_test.dart
+++ test/home_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:jobhop/company/widgets/home.dart';
+import 'package:jobhop/company/pages/home.dart';
 
 
 Widget testWidget = MediaQuery(
