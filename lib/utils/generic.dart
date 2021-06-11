f3aebecb8cdc4d65c23a4d17c1f5c62c40cde5dc
Richard Jansen <richard@pedroja.tech>
2021-06-11 14:32:00 +0200

    added getLocale, setLocale and getUserPk functions

--- lib/utils/generic.dart
+++ lib/utils/generic.dart
@@ -1,6 +1,7 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-Locale? lang2locale(String lang) {
+Locale lang2locale(String lang) {
   if (lang == 'nl') {
     return Locale('nl', 'NL');
   }
@@ -9,9 +10,33 @@ Locale? lang2locale(String lang) {
     return Locale('en', 'US');
   }
 
-  return null;
+  throw("Unknown language: $lang");
 }
 
 String formatDate(DateTime date) {
   return "${date.toLocal()}".split(' ')[0];
+}
+
+Future<Locale> getLocale() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? preferedLanguageCode = prefs.getString('preferedLanguageCode');
+
+  if(preferedLanguageCode == null) {
+    preferedLanguageCode = 'nl';
+  }
+  print('preferedLanguageCode: $preferedLanguageCode');
+
+  return lang2locale(preferedLanguageCode);
+}
+
+Future<void> setLocale(String langCode) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setString('preferedLanguageCode', langCode);
+}
+
+Future<int?> getUserPk() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  final int? userPk = prefs.getInt('userPk');
+
+  return userPk;
 }
