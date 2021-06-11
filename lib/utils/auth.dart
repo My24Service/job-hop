a66cd56c188575154f630cac28b620359709c197
Richard Jansen <richard@pedroja.tech>
2021-06-11 14:29:47 +0200

    added initState method, also store user.id

--- lib/utils/auth.dart
+++ lib/utils/auth.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -14,23 +15,50 @@ GoogleSignIn googleSignIn = GoogleSignIn(
 );
 
 class Auth {
-  Future<String?> getUserToken() async {
+  Future<dynamic> getUserField(String whichField) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
 
-    String? token = prefs.getString('token');
+    String? field = prefs.getString(whichField);
 
-    return token;
+    return field;
+  }
+
+  Future<StudentUser?> initState(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    AppStateModel state = AppStateModel();
+
+    final int? id = prefs.getInt('id');
+    final String? email = prefs.getString('email');
+    final String? username = prefs.getString('username');
+    final String? token = prefs.getString('token');
+
+    if (id == null || email == null || username == null || token == null) {
+      return null;
+    }
+
+    StudentUser user = StudentUser(
+      id: id,
+      email: email,
+      username: username,
+      token: token,
+    );
+
+    state.setUser(user);
+
+    return user;
   }
 
   Future<bool> storeUser(Map<String, dynamic> userData) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     AppStateModel state = AppStateModel();
 
+    await prefs.setString('userPk', userData['id']);
     await prefs.setString('email', userData['email']);
     await prefs.setString('username', userData['username']);
     await prefs.setString('token', userData['token']);
 
     StudentUser user = StudentUser(
+      id: userData['id'],
       email: userData['email'],
       username: userData['username'],
       token: userData['token'],
