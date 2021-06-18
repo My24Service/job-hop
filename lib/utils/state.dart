0246cadcd090b4e343ce34ea4bc3837dee8c262b
Richard Jansen <richard@pedroja.tech>
2021-06-18 09:45:17 +0200

    added set demo in state model

--- lib/utils/state.dart
+++ lib/utils/state.dart
@@ -6,14 +6,25 @@ import 'package:jobhop/company/models/models.dart';
 abstract class AppModel extends ChangeNotifier {
   void setUserBasic(StudentUser user, String token);
   void setUserFull(StudentUser user, String token);
+  void setIsDemo(bool isDemo);
 
   StudentUser get user => user;
+  bool get isDemo => isDemo;
 }
 
 class AppModelImplementation extends AppModel {
   late StudentUser _user;
+  bool _isDemo = false;
 
   AppModelImplementation();
+
+  @override
+  bool get isDemo => _isDemo;
+
+  @override
+  void setIsDemo(bool isDemo) {
+    _isDemo = isDemo;
+  }
 
   @override
   StudentUser get user => _user;
