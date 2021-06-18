7ef2e3634fa6c16ef363d47f21ea4b06433b207a
Richard Jansen <richard@pedroja.tech>
2021-06-18 09:46:36 +0200

    use demo URL when in demo mode

--- lib/core/api/api.dart
+++ lib/core/api/api.dart
@@ -1,9 +1,16 @@
 import 'package:jobhop/utils/generic.dart';
 import 'package:jobhop/core/app_config.dart';
+import 'package:get_it/get_it.dart';
+
+import 'package:jobhop/utils/state.dart';
+
+GetIt getIt = GetIt.instance;
 
 mixin ApiMixin {
   String getUrl(String path) {
-    return "https://${config.apiBaseUrl}$path";
+    final isDemo = getIt<AppModel>().isDemo;
+    final String baseUrl = isDemo ? config.demoApiBaseUrl : config.apiBaseUrl;
+    return "https://$baseUrl$path";
   }
 
   Future<Map<String, String>> getHeaders() async {
