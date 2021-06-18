f3f258d2191d398df9436008fc2d74c2fc91b3dd
Richard Jansen <richard@pedroja.tech>
2021-06-18 09:42:46 +0200

    added requestFCMPermission; setIsDemo/setIsNotDemo

--- lib/utils/generic.dart
+++ lib/utils/generic.dart
@@ -1,5 +1,16 @@
+import 'dart:convert';
+import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get_it/get_it.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
+
+import 'package:jobhop/core/secret.dart';
+import 'package:jobhop/utils/state.dart';
+
+GetIt getIt = GetIt.instance;
 
 Locale lang2locale(String lang) {
   if (lang == 'nl') {
@@ -63,4 +74,66 @@ Future<bool> isFirstTimeProfile() async {
   }
 
   return false;
+}
+
+Future<void> setIsDemo() async {
+  getIt<AppModel>().setIsDemo(true);
+}
+
+Future<void> setIsNotDemo() async {
+  getIt<AppModel>().setIsDemo(false);
+}
+
+Future<void> requestFCMPermissions() async {
+  // request permissions
+  SharedPreferences _prefs = await SharedPreferences.getInstance();
+  _prefs.remove('fcm_allowed');
+
+  if (!_prefs.containsKey('fcm_allowed')) {
+    bool isAllowed = false;
+
+    await Firebase.initializeApp();
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      sound: true,
+      announcement: false,
+      badge: false,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+    );
+
+    // are we allowed?
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      isAllowed = true;
+    }
+
+    _prefs.setBool('fcm_allowed', isAllowed);
+
+    if (isAllowed) {
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        if (message.data.containsKey('demo_okay')) {
+
+        }
+
+        print('Got a message whilst in the foreground!');
+        print('Message data: ${message.data}');
+
+        if (message.notification != null) {
+          print('Message also contained a notification: ${message.notification}');
+        }
+      });
+    }
+  }
+}
+
+String encryptText(String text) {
+  final key = encrypt.Key.fromUtf8(secret.key);
+  // print(base64Url.encode(key.bytes));
+  final fernet = encrypt.Fernet(key);
+  final encrypter = encrypt.Encrypter(fernet);
+  final encryptedToken = encrypter.encrypt(text);
+
+  return encryptedToken.base64;
 }
