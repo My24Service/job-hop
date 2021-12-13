edbe751433b1d126d1ff4a4c96a72d9af60cc2fa
Richard Jansen <richard@pedroja.tech>
2021-12-13 21:16:47 +0100

    fixed tests

--- test/trips_test.dart
+++ test/trips_test.dart
@@ -29,7 +29,7 @@ void main() {
     // return result with a 200
     final String tripsResponse = '{"next":null,"previous":null,"count":1,"num_pages":1,"results":[{"id":9,"description":"tets","required_users":5,"trip_orders":[{"id":18,"order":62,"name":"De Kerstmarktspecialist","address":"Metaalweg 4","postal":"3751 LS","city":"Bunschoten","country_code":"NL","date":"14/06/2021","start_date": "2021-12-06", "start_time": "14:00:00", "end_date": "2021-12-06", "end_time": "17:00:00","modified":"13/06/2021 10:32","created":"13/06/2021 10:32"}],"start_date":"2021-06-21","start_time":"12:15:00","end_date":"2021-06-23","end_time":"13:00:00","modified":"13/06/2021 10:32","created":"13/06/2021 10:32","trip_date":"21/06/2021 12:15 - 23/06/2021 13:00","user_trip_is_available":false,"required_assigned":"0/0 (0.00%)","assigned_user_count":0,"num_orders":1,"users_trip_set_as_available":1,"number_still_available":4}]}';
     when(client.get(
-        Uri.parse('https://jobhop.my24service-dev.com/mobile/trip/'),
+        Uri.parse('https://jobhop.my24service-dev.com/mobile/trip/?page_size=500'),
         headers: anyNamed('headers')))
         .thenAnswer((_) async => http.Response(tripsResponse, 200));
 
