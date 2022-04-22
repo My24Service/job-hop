dfcfbb20e86defc270dad4865f408912ccf67f11
Richard Jansen <richard@pedroja.tech>
2022-04-22 11:48:05 +0200

    remove api from getUrl

--- lib/company/widgets/login.dart
+++ lib/company/widgets/login.dart
@@ -112,12 +112,12 @@ class _LoginViewState extends State<LoginView> {
   }
 
   _passwordReset () async {
-    final url = companyApi.getUrl('/frontend/#/reset-password');
+    final url = companyApi.getUrl('/frontend/#/reset-password').replaceAll('/api', '');
     launch(url);
   }
 
   _register () async {
-    final url = companyApi.getUrl('/old/company/users/student/register/');
+    final url = companyApi.getUrl('/old/company/users/student/register/').replaceAll('/api', '');
     launch(url);
   }
 
